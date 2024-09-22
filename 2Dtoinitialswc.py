17b928b0976f8ada23e9ace29759a7adf60c4e29
Zongmin Liu <118335571+zongminliu9@users.noreply.github.com>
2024-09-22 10:39:14 -0700

    Update and rename 神经3d重建.py to 2Dtoinitialswc.py

--- 2Dtoinitialswc.py
+++ 2Dtoinitialswc.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # Load the image
-image_path = '/Users/zongmin/Downloads/mask5.jpg'
+image_path = ' '
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 # Binarize the image (assume white is the structure and black is the background)
